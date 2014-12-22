493f6830e5e465f89046ab06662c9b609e7cd38f
Grant DeLozier <grantdelozier@gmail.com>
2014-12-22 12:54:48 -0600

    Added command line scripts for python

--- build.sh
+++ build.sh
@@ -11,29 +11,11 @@ while getopts ":l:m:" opt; do
   esac
 done
 
-: ${LEMKIT:?"Need to set LEMKIT non-empty"}
+: ${LEMKIT:?"Need to set LEMKIT variable non-empty"}
 
 printf "Argument lang is %s\n" "$lang"
 printf "Argument mode is %s\n" "$mode"
 printf "LEMKIT environment variable set to $LEMKIT \n"
-
-#if "$lang" == "python"
-#then
-#	if "$mode" == "predict"
-#	then
-#		printf "Building Python Lemkit in predict only mode"
-#		sudo python python/predict/setup.py build
-#		sudo python $LEMKIT/python/predict/setup.py install
-#	elif "$mode" == "train"
-#	then
-#		printf "Building Python Lemkit in train only mode"
-#		sudo python python/train/setup.py build
-#		sudo python $LEMKIT/python/train/setup.py install
-#elif "$lang" == "scala"
-#then
-#	if "$mode" == "predict"
-#	then
-#		printf "Building Scala Lemkit in predict only mode"
 
 case "$lang" in
   *scala*)
@@ -45,11 +27,24 @@ esac
 case "$lang" in
 	*python*)
 	 printf "Building in Python \n"
-	 cd $LEMKIT/python/predict
-	 sudo python setup.py build
-	 sudo python setup.py install
-	 cd $LEMKIT
-	 # code when lang has scala
-	 ;;
+	 case "$mode" in
+	 	*predict*)
+		 printf "Building Predict mode \n"
+		 cd $LEMKIT/python/predict
+		 sudo python setup.py build
+		 sudo python setup.py install
+		 cd $LEMKIT
+		;;
+	 esac
+	 case "$mode" in
+	 	*train*)
+		 printf "Building Train mode \n"
+		 cd $LEMKIT/python/train
+		 sudo python setup.py build
+		 sudo python setup.py install
+		 cd $LEMKIT
+		;;
+	 esac
+	;;
 esac
 		
