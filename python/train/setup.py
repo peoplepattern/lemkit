aeb44f53c8763c28ae180ed3a9eead6eb07436f4
Grant DeLozier <grantdelozier@gmail.com>
2014-12-22 16:02:53 -0600

    Removed nonexistent script from setup file

--- python/train/setup.py
+++ python/train/setup.py
@@ -12,7 +12,6 @@ config = {
   'version': '0.1',
   'install_requires': ['nose','mmh3', 'scipy', 'numpy', 'scikit-learn'],
   'packages': ['lemkit_train'],
-  'scripts': ['bin/commons-predict'],
   'name': 'lemkit_train'
 }
 
