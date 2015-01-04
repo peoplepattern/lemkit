08750c1f5a653b427afe94ff2f309da71754edfa
Grant DeLozier <grantdelozier@gmail.com>
2015-01-04 16:18:38 -0600

    Added -l option to conform-tests.sh

--- conform-test.sh
+++ conform-test.sh
@@ -2,44 +2,71 @@
 
 : ${LEMKIT:?"Need to set LEMKIT variable non-empty"}
 
-if $LEMKIT/scala/bin/lkpredict -m $LEMKIT/data/model_files/iris.vw.model.json -p $LEMKIT/data/iris/iris.test.txt -f json > /tmp/iris-predictions.txt ; then
-	printf "SCALA JSON \n"
-	cmp $LEMKIT/data/conform/iris-expected-predictions.txt /tmp/iris-predictions.txt
-else
-	printf "Scala is not built properly for predicting, skipping \n"
-fi
+printf "LEMKIT environment variable set to $LEMKIT \n"
 
-if $LEMKIT/python/bin/lkpredict -m $LEMKIT/data/model_files/iris.vw.model.json -p $LEMKIT/data/iris/iris.test.txt -f json > /tmp/iris-predictions.txt ; then
-	printf "PYTHON JSON \n"
-	cmp $LEMKIT/data/conform/iris-expected-predictions.txt /tmp/iris-predictions.txt
-else
-	printf "Python is not built properly for predicting, skipping \n"
-fi
+while getopts ":l:" opt; do
+  case $opt in
+    l) lang="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
 
-if $GOPATH/bin/lkpredict -m $LEMKIT/data/model_files/iris.vw.model.json -p $LEMKIT/data/iris/iris.test.txt -f json > /tmp/iris-predictions.txt ; then
-	printf "GO JSON \n"
-	cmp $LEMKIT/data/conform/iris-expected-predictions.txt /tmp/iris-predictions.txt
-else
-	printf "Go is not built properly for predicting, skipping \n"
-fi
+case "$lang" in
+  *scala*)
+	if $LEMKIT/scala/bin/lkpredict -m $LEMKIT/data/model_files/iris.vw.model.json -p $LEMKIT/data/iris/iris.test.txt -f json > /tmp/iris-predictions.txt ; then
+		printf "SCALA JSON \n"
+		cmp $LEMKIT/data/conform/iris-expected-predictions.txt /tmp/iris-predictions.txt
+	else
+		printf "Scala is not built properly for predicting, skipping \n"
+	fi
 
-if $LEMKIT/scala/bin/lkpredict -m $LEMKIT/data/model_files/iris.vw.model.bin -p $LEMKIT/data/iris/iris.test.txt -f binary > /tmp/iris-predictions.txt ; then
-	printf "SCALA BINARY \n"
-	cmp $LEMKIT/data/conform/iris-expected-predictions.txt /tmp/iris-predictions.txt
-else
-	printf "Scala is not built properly for predicting, skipping \n"
-fi
+	if $LEMKIT/scala/bin/lkpredict -m $LEMKIT/data/model_files/iris.vw.model.bin -p $LEMKIT/data/iris/iris.test.txt -f binary > /tmp/iris-predictions.txt ; then
+		printf "SCALA BINARY \n"
+		cmp $LEMKIT/data/conform/iris-expected-predictions.txt /tmp/iris-predictions.txt
+	else
+		printf "Scala is not built properly for predicting, skipping \n"
+	fi
+  ;;
+esac
 
-if $LEMKIT/python/bin/lkpredict -m $LEMKIT/data/model_files/iris.vw.model.bin -p $LEMKIT/data/iris/iris.test.txt -f binary > /tmp/iris-predictions.txt ; then
-	printf "PYTHON BINARY \n"
-	cmp $LEMKIT/data/conform/iris-expected-predictions.txt /tmp/iris-predictions.txt
-else
-	printf "Python is not built properly for predicting, skipping \n"
-fi
+case "$lang" in
+  *python*)
+	if $LEMKIT/python/bin/lkpredict -m $LEMKIT/data/model_files/iris.vw.model.json -p $LEMKIT/data/iris/iris.test.txt -f json > /tmp/iris-predictions.txt ; then
+		printf "PYTHON JSON \n"
+		cmp $LEMKIT/data/conform/iris-expected-predictions.txt /tmp/iris-predictions.txt
+	else
+		printf "Python is not built properly for predicting, skipping \n"
+	fi
 
-if $GOPATH/bin/lkpredict -m $LEMKIT/data/model_files/iris.vw.model.bin -p $LEMKIT/data/iris/iris.test.txt -f binary > /tmp/iris-predictions.txt ; then
-	printf "GO BINARY \n"
-	cmp $LEMKIT/data/conform/iris-expected-predictions.txt /tmp/iris-predictions.txt
-else
-	printf "Go is not built properly for predicting, skipping \n"
-fi
+	if $LEMKIT/python/bin/lkpredict -m $LEMKIT/data/model_files/iris.vw.model.bin -p $LEMKIT/data/iris/iris.test.txt -f binary > /tmp/iris-predictions.txt ; then
+		printf "PYTHON BINARY \n"
+		cmp $LEMKIT/data/conform/iris-expected-predictions.txt /tmp/iris-predictions.txt
+	else
+		printf "Python is not built properly for predicting, skipping \n"
+	fi
+  ;;
+esac
+
+case "$lang" in
+  *go*)
+	if $GOPATH/bin/lkpredict -m $LEMKIT/data/model_files/iris.vw.model.json -p $LEMKIT/data/iris/iris.test.txt -f json > /tmp/iris-predictions.txt ; then
+		printf "GO JSON \n"
+		cmp $LEMKIT/data/conform/iris-expected-predictions.txt /tmp/iris-predictions.txt
+	else
+		printf "Go is not built properly for predicting, skipping \n"
+	fi
+
+	if $GOPATH/bin/lkpredict -m $LEMKIT/data/model_files/iris.vw.model.bin -p $LEMKIT/data/iris/iris.test.txt -f binary > /tmp/iris-predictions.txt ; then
+		printf "GO BINARY \n"
+		cmp $LEMKIT/data/conform/iris-expected-predictions.txt /tmp/iris-predictions.txt
+	else
+		printf "Go is not built properly for predicting, skipping \n"
+	fi
+  ;;
+esac
+
+
+
+
